8f315cddb0c42c51cb0ea9c24537c0e85849244c
HackyRaji <hackyraji02@gmail.com>
2024-09-04 18:18:00 +0100

    about me page

--- src/app/about-me/page.jsx
+++ src/app/about-me/page.jsx
@@ -2,8 +2,9 @@
 
 export default function AboutMe() {
   return (
-    <div>
-      <h1>This is about me</h1>
+    <div className="px-24 pt-16">
+      <h1 className="text-1xl font-bold mb-10">About Me</h1>
+      <p>I have lived among my people in our community since birth, deeply understanding our shared values and challenges. I am a hardworking entrepreneur who views farming as a vital business, and I am committed to transforming it into a sustainable source of prosperity for all. My dedication to community development is evident in my tireless efforts to uplift those around me. As a friend to all, I strive to build bridges, foster unity, and create opportunities that benefit every member of our community. Together, we can forge a brighter future.</p>
     </div>
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,17 +2,14 @@
 import Image from "next/image";
 const Hero = () => {
   return (
-    <div className='bg-[#120800] h-screen flex flex-col lg:flex-row justify-center lg:justify-between items-center px-3 lg:px-32 py-14 bg-img'>
+    <div className='bg-[#120800] h-[calc(100vh-100px)] flex flex-col lg:flex-row justify-center lg:justify-between items-center px-3 lg:px-32  bg-img'>
       <div className="text-[#FEFEFE] w-1/2">
-      <p className="leading-[1.3rem] text-[#CBE3D2]">I am <span className="text-[#018C50] font-bold">Opayooye Abiodun</span> , I am seeking for your support
-        in the upcoming local government election as councilor
-        for Ira ward.
+      <p className="leading-[1.3rem] text-[#CBE3D2]">I, <span className="text-[#018C50] font-bold">Opayooye Abiodun.</span> am seeking your invaluable support in the upcoming local government election as a councillor candidate for Ira Ward, to work together towards a brighter, more prosperous future 
+      for our community.
       </p>
-        <h1 className="leading-[2rem] text-3xl font-bold">TOGETHER WE CAN ELEVATE OUR DEAR HABITABLE,
-          ACIENT HOME OF OYA TO ITS RIGHFUL PLACE. <span className="underline">IRA MAJA</span> .
+        <h1 className="leading-[2rem] text-3xl font-bold">TOGETHER, WE CAN ELEVATE OUR DEAR  CHERISHED, ANCIENT HOME OF OYA, IRA, TO ITS RIGHT PLACE OF HOURNOR. <span className="underline">IRA MAJA!</span> .
         </h1>
-        <p className="mt-4 text-sm text-[#CBE3D2]">No one will and no one can build it for us except if we stand up 
-          and show the world we can. yes we can together.
+        <p className="mt-4 text-sm text-[#CBE3D2]">No one will build it for us, and no one can—unless we stand up and show the world our strength. Yes, we can, and together, we will
         </p>
       </div>
       <Image 
